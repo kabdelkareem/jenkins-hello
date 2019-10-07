3ebefb5f75eea6821822703960a519fe15927a72
Karim Abd ElKareem <abdelkareem2010@gmail.com>
2019-10-07 22:21:43 +0300

    update repo

--- src/main/java/com/karim/examples/Main.java
+++ src/main/java/com/karim/examples/Main.java
@@ -1,7 +1,15 @@
 package com.karim.examples;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+	private final String message = "Hello World!";
+
+    public Main() {}
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
 }
---
+++ src/test/java/com/karim/examples/MainTest.java
@@ -0,0 +1,48 @@
+package com.karim.examples;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple Main.
+ */
+public class MainTest
+{
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testAppConstructor() {
+        try {
+            new Main();
+        } catch (Exception e) {
+            fail("Construction failed.");
+        }
+    }
+
+    @Test
+    public void testAppMain()
+    {
+        Main.main(null);
+        try {
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+        } catch (AssertionError e) {
+            fail("\"message\" is not \"Hello World!\"");
+        }
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+}
