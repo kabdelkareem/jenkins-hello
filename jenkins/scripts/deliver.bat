13aba4768dc94689957d14e09c7dbdec96fb2532
Karim Abd ElKareem <41565147+kabdelkareem@users.noreply.github.com>
2019-10-08 22:47:23 +0300

    Update deliver.bat

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -8,11 +8,11 @@ mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
-set NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+NAME=$(mvn help:evaluate -Dexpression=project.name -q -DforceStdout)
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
-set VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
